0539be11c7111a27443c146ab0f1f394ad6bd13e
Rusbel Gaviria <kimar041604@gmail.com>
2020-09-02 22:58:29 -0500

    se creo archivo json.html

---
+++ json.js
@@ -0,0 +1,24 @@
+//Arreglos en JSON
+//JavaScripts Object Notation
+
+var contacts = {
+    "PhoneBook":{
+        "object":String,
+        "owner":{
+            "Contact":{
+                "objectid":String,
+                "phone":String,
+                "name":String,
+                "title":String,
+                "age":Number,
+                "address":{
+                    "objectid":String,
+                    "street":String,
+                    "city":String,
+                    "state":String,
+                }
+        },
+        "contacts":contact[],
+    }
+    }
+}
